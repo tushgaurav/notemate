2978a65bb76502cb1c44997aa0003aad6850c829
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2022-08-18 07:51:13 +0530

    added create note

--- notes/urls.py
+++ notes/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('user/<username>', views.profileView, name='profile'),
+    path('add/', views.createNote, name='add_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from .models import Note
+from .forms import NoteForm
 
 def home(request):
     notes = Note.objects.all()
@@ -8,3 +9,26 @@ def home(request):
         'notes': notes
     }
     return render(request, 'notes/home.html', context)
+
+def profileView(request, username):
+    user = User.objects.get(username=username)
+    context = {
+        'user': user
+    }
+    return render(request, 'notes/user.html', context)
+
+def createNote(request):
+    notes = Note.objects.all()
+    form = NoteForm()
+    context = {
+        'form': form
+    }
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect('home')
+    return render(request, 'notes/add_note.html', context)
